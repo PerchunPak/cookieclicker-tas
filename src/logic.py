62dec864b4bdc596d8eebf74706b1d09dc48b131
PerchunPak <git@perchun.it>
2024-02-21 14:48:00 +0100

    Add some explanation for yesterday's improvement

--- src/logic.py
+++ src/logic.py
@@ -114,6 +114,13 @@ class Logic:
         for i, building in enumerate(buildings):
             produces_per_second = building.produces
 
+            # sometimes we get buildings that we didn't buy yet, and so
+            # we can't know how many they produce. so we estimate that
+            # they produce 10 times more than the previous building.
+            #
+            # but previous building might also not be bought yet, so we
+            # need to go back until we find a building that we bought,
+            # and we set `produces_per_second` to be 10^shift times
             shift = 0
             for shift in itertools.count():
                 if buildings[i - shift].produces is not None:
@@ -124,6 +131,8 @@ class Logic:
             payback_period = building.costs / produces_per_second
 
             if len(coefficients) >= 2:
+                # if time to earn money for the building is longer than a payback
+                # period of the previous building, then it is not worth buying it
                 time_to_earn = (building.costs - self.balance) / self.produced_per_last_second
                 profitability_of_second_best = sorted(coefficients.values())[1]
                 if building.costs > self.balance and time_to_earn > (profitability_of_second_best / 2):
@@ -132,7 +141,6 @@ class Logic:
             coefficients[building.id] = payback_period
 
         the_best_building_id = min(coefficients.items(), key=lambda x: x[1])[0]
-        logger.trace(f"{the_best_building_id=}")
         for building in buildings:
             if building.id == the_best_building_id:
                 return building
